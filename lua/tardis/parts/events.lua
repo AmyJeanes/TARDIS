e8d384427037d00dbf786d413b78571e08c0c3a7
Amy Jeanes <amyjeanes23@gmail.com>
2025-10-22 00:34:34 +0100

    add tardis pumpkin part/model (WIP)

---
+++ lua/tardis/parts/events.lua
@@ -0,0 +1,18 @@
+-- Events
+
+local PART={}
+PART.ID = "pumpkin"
+PART.AutoSetup = true
+PART.Collision = true
+PART.ShouldTakeDamage = false
+PART.Motion = true
+PART.Model = "models/molda/misc/tardis_pumpkin.mdl"
+PART.InvisibleCollision = false
+
+if CLIENT then
+    function PART:Use(ply)
+        self.exterior:NotifyEvent(false, true)
+    end
+end
+
+TARDIS:AddPart(PART)
